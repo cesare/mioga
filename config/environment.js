bb3b70e910dc06a445a0ad469b6aa7f0e1c691d7
SAWADA Tadashi <cesare@mayverse.jp>
2012-01-29 10:24:49 +0900

    enabled Twitter authentication.

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,15 @@
 var express    = require('express');
+var everyauth  = require('everyauth');
+var mioga      = require('./mioga')[app.settings.env];
+
+everyauth.twitter
+  .consumerKey(mioga.twitter.consumerKey)
+  .consumerSecret(mioga.twitter.consumerSecret)
+  .findOrCreateUser( function (session, accessToken, accessTokenSecret, twitterUserMetadata) {
+	  return {name: "dummy"};
+  })
+  .entryPath('/auth/twitter')
+  .redirectPath('/');
 
 app.configure(function(){
     var cwd = process.cwd();
@@ -11,5 +22,8 @@ app.configure(function(){
     app.use(express.cookieParser());
     app.use(express.session({secret: 'secret'}));
     app.use(express.methodOverride());
+    app.use(everyauth.middleware());
     app.use(app.router);
 });
+
+everyauth.helpExpress(app);
